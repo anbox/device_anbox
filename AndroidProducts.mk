3dbcf745691608c687988067757538e56ed12c2f
Marius Gripsgard <mariogrip@ubuntu.com>
2017-01-27 23:55:31 +0100

    Rename armv7_neon to match PRODUCT_NAME
    PRODUCT_NAME and the product makefile name have to match for lunch to locate the product makefile.

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,4 +1,4 @@
 PRODUCT_MAKEFILES := \
 	$(LOCAL_DIR)/anbox_desktop_x86_64.mk \
-	$(LOCAL_DIR)/anbox_desktop_armv7a_neon.mk \
+	$(LOCAL_DIR)/anbox_desktop_arm.mk \
 	$(LOCAL_DIR)/anbox_desktop_arm64.mk
